d18d096766aa47fc539506a0a8211a0ca6f9d382
sahilq312 <quraishisahil04@outlook.com>
2024-07-13 19:28:14 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InputComponent } from './input/input.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'dashboard',
+        component : DashboardComponent
+    },
+    {
+        path : 'input',
+        component : InputComponent
+    }
+];
